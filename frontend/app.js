c0c5f56717942093fe4735b23a0d61d00c1807b4
Abishek Arumugam <abishek190507@gmail.com>
2026-09-03 22:45:05 +0530

    Update contract address in app.js

--- frontend/app.js
+++ frontend/app.js
@@ -2,7 +2,7 @@ import { ethers } from "https://esm.sh/ethers@6.13.1";
 
 
 // CONFIG: Paste your contract address and ABI from Remix here
-const CONTRACT_ADDRESS = "0x6420da67851e4c49F08F7726B3E7d60b0722867D"; // <-- REPLACE with your deployed contract address
+const CONTRACT_ADDRESS = "0xd8b934580fcE35a11B58C6D73aDeE468a2833fa8"; // <-- REPLACE with your deployed contract address
 const ABI =  [
 		{
 			"inputs": [
